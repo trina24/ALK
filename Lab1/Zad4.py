00eeaecc1c26dbb1fe89cf3ede76b1ec676d2bbc
trina24 <katarzyna_kalek@wp.pl>
2016-03-01 23:47:37 +0100

    Lab1-4-komentarze

--- Lab1/Zad4.py
+++ Lab1/Zad4.py
@@ -4,25 +4,25 @@ import math
 class Generator:
 
     def __init__(self, n):
-        self.n = n
-        self.ranklist = [0 for i in range(pow(2,n))]
+        self.n = n #liczba elementów wyjściowego zbioru
+        self.ranklist = [0 for i in range(pow(2,n))] #inicjalizacja listy wektorów charakterystycznych
     
     def fill_ranks(self, n):
         for i in range(len(self.ranklist)):
+            #zamiana rangi na ciąg binarny
             binary = bin(i)[2:]
+            #dopisanie zer z przodu
             for j in range(n-len(binary)):
                 binary = '0' + binary
+            #wypełnienie listy ciągami
             self.ranklist[i] = binary
         return self.ranklist
     
-    def printing(self):
-        toprint = self.fill_ranks(self.n)
-        print toprint
-
     def generate(self):
         for element in self.fill_ranks(self.n):
             binary = str(element)
             myset = []
+            #jeżeli na dla danej liczby na jej pozycji w ciągu jest jedynka, to wrzucamy ją do zbioru
             for i in range(len(binary)):
                 if (int(binary[i]) == 1):
                     myset.append(i+1)
@@ -33,4 +33,3 @@ class Main:
     n = int(raw_input('Podaj n: \n'))
     gen = Generator(n)
     gen.generate()
-    #gen.printing()
