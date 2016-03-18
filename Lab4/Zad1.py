e6782e036277ee5cf4fd81ecdbf8af3736494b3c
trina24 <katarzyna_kalek@wp.pl>
2016-03-18 16:25:54 +0100

    Lab4 Zad1 - poprawione

--- Lab4/Zad1.py
+++ Lab4/Zad1.py
@@ -5,49 +5,34 @@
 # k-elementowych.
 
 
-class Generator:
+class Successor:
 
-    def __init__(self, n, k):
+    def __init__(self, n, subset):
         self.n = n
-        self.k = k
-        self.counter = k-1  # licznik ustawiamy na koniec wektora
-        self.vector = [i + 1 for i in range(k)]  # inicjujemy 1,...,k
-        self.stop = False  # warunek stopu
+        self.k = len(subset)
+        self.vector = [int(element) for element in subset]
 
-    def complete(self, vector, counter):
-    # zwiększa elementy na końcu wektora
-        while (vector[counter] < self.n - self.k + counter + 1):
-            vector[counter] += 1
-            print vector
-        self.vector = vector
-
-    def reset(self, vector, counter):
-        # przesuwa licznik w lewo, tak długo, jak wszystko jest
-        # "zrobione"
+    def successor(self):
+        counter = self.k - 1
+        vector = self.vector
+        # przesuwa licznik w lewo, tak długo, aż będzie się dało
+        # zwiększyć element na danej pozycji
         while (vector[counter] == self.n - self.k + counter + 1):
             counter -= 1
-            # jeśli przejdziemy cały wektor, to koniec
-            if (counter < 0):
-                return True
-        # w przeciwnym wypadku zwiększamy bieżący element i wstawiamy
-        # kolejne liczby naturalne na prawo
-        vector[counter] += 1
-        for i in range(counter + 1, len(vector)):
-            vector[i] = vector[i - 1] + 1
-        print vector
-        self.vector = vector
-        return False
-    
-    def generate(self):
-        print self.vector
-        stop = self.stop
-        while (stop == False):
-            self.complete(self.vector, self.counter)
-            stop = self.reset(self.vector, self.counter)
+        # jeśli przejdziemy cały wektor, to koniec
+        if (counter < 0):
+            return 'Brak następnika'
+        else:
+        # w przeciwnym wypadku zwiększamy bieżący element
+        # i wstawiamy kolejne liczby naturalne na prawo
+            vector[counter] += 1
+            for i in range(counter + 1, len(vector)):
+                vector[i] = vector[i - 1] + 1
+            return vector
 
 class Main:
 
     n = int(raw_input('Podaj n: \n'))
-    k = int(raw_input('Podaj k: \n'))
-    gen = Generator(n, k)
-    gen.generate()
+    subset = raw_input('Podaj elementy podzbioru: \n').split()
+    sc = Successor(n, subset)
+    print sc.successor()
